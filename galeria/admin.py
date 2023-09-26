4953b2751aefff1318b1f85ae50792ad1521637c
tniagogluszczak <thiago.gluszczak@escola.pr.gov.br>
2023-09-26 18:09:26 -0300

    Projeto Alura Space 2

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Photo
 
-# Register your models here.
+class ListPhotos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'publicada')
+    list_display_links = ('id', 'nome', 'legenda')
+    search_fields = ('nome',)
+    list_filter = ('category',)
+    list_editable = ('publicada',)
+    list_per_page = 10
+
+admin.site.register(Photo, ListPhotos)
--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Photo
 
 def index(request):
-    return render(request, 'AluraSPACE/index.html')
+    Fotos = Photo.objects.order_by('data').filter(publicada=True)
+    return render(request, 'AluraSPACE/index.html', {'cards': Fotos})
 
-def imagem(request):
-    return render(request, 'AluraSPACE/imagem.html')
+def imagem(request, photos_id):
+    photo = get_object_or_404(Photo, pk=photos_id)
+    return render(request, 'AluraSPACE/imagem.html', {'photos': photo})
+
+def search(request):
+    Fotos = Photo.objects.order_by('data').filter(publicada=True)
+
+    if 'search' in request.GET: 
+        GetPhoto = request.GET['search']
+        if GetPhoto: Fotos = Photo.objects.filter(nome__icontains = GetPhoto)
+
+    return render(request, 'AluraSPACE/search.html', {'cards': Fotos})
